b360e47eb4e99e8fd363ea880db8191cd3a45af1
mehdi <mehdi@seald.io>
2022-09-29 15:57:05 +0200

    doc detail

--- src/webcrypto/utils.ts
+++ src/webcrypto/utils.ts
@@ -46,7 +46,7 @@ export const sha256 = (data: Buffer): Buffer => {
 
 /**
  * Returns a Buffer of random bytes
- * @param {number} [length=10]
+ * @param {number} [length=10] - limited to 65536 bytes
  * @return {Buffer}
  */
 export const randomBytes = (length = 10): Buffer => {
@@ -61,7 +61,7 @@ export const randomBytes = (length = 10): Buffer => {
 
 /**
  * Returns a Buffer of random bytes
- * @param {number} [length=10]
+ * @param {number} [length=10] - limited to 65536 bytes
  * @return {Promise<Buffer>}
  */
 export const randomBytesAsync = async (length = 10): Promise<Buffer> => {
